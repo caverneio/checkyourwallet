49de8b5e7c44f0ed39ba52121d41bd3dd0eea029
Anthony Ivan <83598208+4nthonycuev4@users.noreply.github.com>
2022-08-05 22:02:10 -0500

    Solved delete item

--- pages/api/items/[date]/[id].js
+++ pages/api/items/[date]/[id].js
@@ -1,16 +1,19 @@
 import FaunaClient from "fauna";
+import { withApiAuthRequired, getAccessToken } from "@auth0/nextjs-auth0";
 
-export default async function handler(req, res) {
+async function handler(req, res) {
   try {
-    if (req.method === "DELETE") {
+    const {accessToken} = await getAccessToken();
+    const client = new FaunaClient();
+  if (req.method === "DELETE") {
       const { id } = req.query;
-      const client = new FaunaClient();
-      const items = await client.deleteItem(id);
-
-      res.status(200).json(items);
+      const item = await client.deleteItem(id);
+      res.status(200).json(item);
     }
   } catch (error) {
     console.log("error", error);
     res.status(500).json({ error });
   }
 }
+
+export default withApiAuthRequired(handler);
