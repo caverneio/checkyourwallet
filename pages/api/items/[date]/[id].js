b1b1ade18bdc3600d3f7099b5473a0a21116ea52
Anthony Ivan <83598208+4nthonycuev4@users.noreply.github.com>
2022-08-05 22:08:03 -0500

    Update [id].js

--- pages/api/items/[date]/[id].js
+++ pages/api/items/[date]/[id].js
@@ -3,7 +3,7 @@ import { withApiAuthRequired, getAccessToken } from "@auth0/nextjs-auth0";
 
 async function handler(req, res) {
   try {
-    const {accessToken} = await getAccessToken();
+    const {accessToken} = await getAccessToken(accessToken);
     const client = new FaunaClient();
   if (req.method === "DELETE") {
       const { id } = req.query;
